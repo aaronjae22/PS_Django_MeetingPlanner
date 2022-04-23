c184bd292ecde5a495378564641e818dd27fc93a
Aaron Ayerdis <ayerdis.espinoza.aa@gmail.com>
2022-04-23 14:07:13 -0600

    4. Adding testing views functions

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,6 +1,15 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
+
 
 # Create your views here.
 def welcome(request):
     return HttpResponse("Welcome to the Meeting Planner!")
+
+def date(request):
+    return HttpResponse("This page was server at " + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("Project created by @aayerdesp")
