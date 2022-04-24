76252c680d09d428d088ecf847aecdc3819f4a97
Aaron Ayerdis <ayerdis.espinoza.aa@gmail.com>
2022-04-24 11:40:03 -0600

    11. Adding Room model class and a reference to Meeting model class

--- meeting_planner/meetings/admin.py
+++ meeting_planner/meetings/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Import our class #
-from .models import Meeting
+from .models import Meeting, Room
 
 # Register your models here.
 admin.site.register(Meeting)
+admin.site.register(Room)
