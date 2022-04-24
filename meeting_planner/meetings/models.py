6b796688e17b25d6c7e0501c8d668da2484f08cc
Aaron Ayerdis <ayerdis.espinoza.aa@gmail.com>
2022-04-24 11:29:04 -0600

    9. Adding start_time and duration to Meeting model class

--- meeting_planner/meetings/models.py
+++ meeting_planner/meetings/models.py
@@ -1,3 +1,5 @@
+from datetime import time
+
 from django.db import models
 
 
@@ -5,4 +7,6 @@ from django.db import models
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
 
