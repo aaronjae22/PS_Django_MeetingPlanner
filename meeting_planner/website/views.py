bb1e33130a4a3bee432281b98873f8d5f07e68a9
Aaron Ayerdis <ayerdis.espinoza.aa@gmail.com>
2022-04-26 23:11:53 -0600

    14. Retrieving number of meetings with dynamic content and objects.count()

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -3,13 +3,14 @@ from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from meetings.models import Meeting
 
 # Create your views here.
 def welcome(request):
     # return HttpResponse("Welcome to the Meeting Planner!")
     # return render(request, "website/welcome.html")
     return render(request, "website/welcome.html",
-                  {"message": "This data was sent from the view to the template."})
+                  {"num_meetings": Meeting.objects.count()})
 
 def date(request):
     return HttpResponse("This page was server at " + str(datetime.now()))
