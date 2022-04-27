0bc9f41109dee4a11d3d44cd513807e41d367f88
Aaron Ayerdis <ayerdis.espinoza.aa@gmail.com>
2022-04-26 20:21:58 -0600

    13. Template variables and dynamic content

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -7,7 +7,9 @@ from django.http import HttpResponse
 # Create your views here.
 def welcome(request):
     # return HttpResponse("Welcome to the Meeting Planner!")
-    return render(request, "website/welcome.html")
+    # return render(request, "website/welcome.html")
+    return render(request, "website/welcome.html",
+                  {"message": "This data was sent from the view to the template."})
 
 def date(request):
     return HttpResponse("This page was server at " + str(datetime.now()))
